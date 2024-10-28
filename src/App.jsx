3a15a820184c94a9e5f7419de87486a43ded240d
hase218 <chju23041@g.nihon-u.ac.jp>
2024-10-28 12:56:40 +0900

    Itemの配列を作成 useStateでx,y,zItemが変わるようにした 初期値が空だったら名前がbobになるようにした

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
+import{ useState } from "react";
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
 export default function App() {
-    const showStory = false;
-    const xItem = "";
-    const yItem = "";
-    const zItem = "";
+    const [showStory, setShowStory] = useState(false);
+    const x = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+    const y = ["the soup kitchen", "Disneyland", "the White House"];
+    const z = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+    
+    const [name, setName] = useState("");
+    const [xItem, setXItem] = useState(randomValueFromArray(x));
+    const [yItem, setYItem] = useState(randomValueFromArray(y));
+    const [zItem, setZitem] = useState(randomValueFromArray(z));
+
     const ukus = "us";
+
+    function handleClickResetButton(){
+      console.log("click");
+      setXItem(randomValueFromArray(x));
+      setYItem(randomValueFromArray(y));
+      setZitem(randomValueFromArray(z));
+      setShowStory(true);
+      
+    }
+    console.log(xItem, yItem, zItem);
     return (
       <>
         <div>
           <label htmlFor="customname">Enter custom name:</label>
-          <input type="text" placeholder="" />
+          <input value={name}onChange={(event)=>{setName(event.target.value)}}type="text" placeholder="" />
         </div>
         <div>
           <label htmlFor="us">US</label>
@@ -17,13 +40,13 @@ export default function App() {
           <input type="radio" value="uk" checked={ukus === "uk"} />
         </div>
         <div>
-          <button>Generate random story</button>
+          <button onClick={handleClickResetButton}>Generate random story</button>
         </div>
         {showStory && (
           <p>
             It was 94 fahrenheit outside, so {xItem} went for a walk. When they
             got to {yItem}, they stared in horror for a few moments, then {zItem}.
-            Bob saw the whole thing, but was not surprised — {xItem} weighs 300
+            {name == "" ? "Bob" : name} saw the whole thing, but was not surprised — {xItem} weighs 300
             pounds, and it was a hot day.
           </p>
         )}
